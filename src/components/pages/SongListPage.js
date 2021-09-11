ad7a83d498405561050a0bba6f559fe431b091fd
jow631s <66857116+jow631s@users.noreply.github.com>
2021-09-11 12:42:43 -0500

    add first dummy component

---
+++ src/components/pages/SongListPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SongListPage = () => {
+    return (
+        <div>
+            Keeney and Wyrick build a setlist-builder app
+        </div>
+    )
+}
+
+export default SongListPage;
